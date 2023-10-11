1d4b22c103e28a95fd437d997fe56e09812300e3
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-10-11 16:20:35 +0530

    first commit

---
+++ pages/[meetupID]/index.js
@@ -0,0 +1,9 @@
+import meetupDetail from "../../components/meetups/MeetupDetail";
+
+function meetupDetails(){
+    return(
+        <meetupDetail image = "https://upload.wikimedia.org/wikipedia/commons/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg"  title = "Firdt Meetup"  address="Street 5, Some city" description="Our First MeetUp"/>
+    ) 
+}
+
+export default meetupDetails;
---
+++ pages/index.js
@@ -0,0 +1,26 @@
+
+import MeetupList from "../components/meetups/MeetupList";
+
+
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meet',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg',
+        address: 'Some address 5, 1234 Some city',
+        description: 'This is a first meet up!'
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meet',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg',
+        address: 'Some address 10, 5678 Some city',
+        description: 'This is a second meet up!'
+    },
+]
+function MeetUp() {
+    return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export default MeetUp;
